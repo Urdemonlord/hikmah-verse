86d23a20195af1950171d149e00510b71f3b784c
Urdemonlord <hasrinata@gmail.com>
2024-10-10 11:27:28 +0700

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path'); // Tambahkan import path di sini
 const app = express();
 const PORT = process.env.PORT || 3000;
 
